e4b7a2fcb19dffc3e6c43ff04a5068e5dda5cfe2
rodrigo <rodrigo@gato>
2016-12-08 21:06:56 -0300

    Implemented training and validation with descriptor vectors

--- TrainDataExtractor/extractor.py
+++ TrainDataExtractor/extractor.py
@@ -17,13 +17,25 @@ import matplotlib.pyplot as plt
 ################## APP'S CONFIG ##################
 LOG_LEVEL = logging.INFO
 USE_VECTOR = True
-TEST_PREDICTIONS = True
+SHOW_MATRIX = True
+SHOW_STATS = True
 OUTPUT_DIR = './output/'
 
 ##################################################
 logger = None
 nsplit = None
 nclusters = 7
+
+
+##################################################
+def getBaseName(inputdir_, outdir_):
+	i = -1
+	fname = inputdir_.split('/')[i]
+	while len(fname) == 0:
+		i = i -1
+		fname = inputdir_.split('/')[i]
+
+	return outdir_ + fname
 
 
 ##################################################
@@ -92,7 +104,7 @@ def traverseDirectory(input_, fields_, csvwriter_ = None):
 		element = input_ + f
 
 		if (os.path.isdir(element)):
-			logger.info('...traversing ' + f)
+			logger.debug('...traversing ' + f)
 			dat, resp, nf, ns = traverseDirectory(element + '/', fields_, csvwriter_)
 
 			if len(dat) > 0:
@@ -106,7 +118,7 @@ def traverseDirectory(input_, fields_, csvwriter_ = None):
 			if not f.split('.')[-1].lower() == 'yaml':
 				continue
 
-			logger.info('...extracting ' + f)
+			logger.debug('...extracting ' + f)
 			nfiles = nfiles + 1
 
 			try:
@@ -160,6 +172,153 @@ def extractToCSV(inputdir_, foutput_, csvfields_):
 		logger.info('Unable to create output file')
 
 
+##################################################
+def trainSVM(input_, response_):
+	logger.info('...training SVM')
+
+	svmParams = dict(kernel_type = cv2.SVM_RBF, svm_type = cv2.SVM_C_SVC, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001))
+
+	svm_auto = cv2.SVM()
+	svm_auto.train_auto(input_, response_, None, None, params=svmParams, k_fold=7)
+
+	svmParams['C'] = 100
+	svmParams['gamma'] = 2
+	svm = cv2.SVM()
+	svm.train(input_, response_, params=svmParams)
+
+	return [svm, svm_auto]
+
+
+##################################################
+def trainBoost(input_, response_):
+	logger.info('...training boosted classifier')
+
+	boostParams = dict(boost_type = cv2.BOOST_REAL, weak_count = 100, weight_trim_rate = 0.95, cv_folds = 3, max_depth = 1)
+
+	boost = cv2.Boost()
+	boost.train(trainData=input_, tflag=cv2.CV_ROW_SAMPLE, responses=response_, params=boostParams, update=False)
+
+	return boost
+
+
+##################################################
+def trainNetwork(input_, response_):
+	logger.info('...training neural network')
+	# networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_BACKPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001), bp_dw_scale=0.1, bp_moment_scale=0.1, flags=(cv2.ANN_MLP_UPDATE_WEIGHTS | cv2.ANN_MLP_NO_INPUT_SCALE))
+	
+	networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_BACKPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 5000, 0.000001), bp_dw_scale=0.1, bp_moment_scale=0.1)
+
+
+	# networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_RPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001), rp_dw0=0.1, rp_dw_plus=1.2, rp_dw_minus=0.5, rp_dw_min=0.1, rp_dw_max=50, flags=(cv2.ANN_MLP_UPDATE_WEIGHTS | cv2.ANN_MLP_NO_INPUT_SCALE))
+
+	# networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_RPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001), rp_dw0=0.1, rp_dw_plus=1.2, rp_dw_minus=0.5, rp_dw_min=0.1, rp_dw_max=50)
+
+
+	network = cv2.ANN_MLP()
+	network.create(layerSizes=numpy.array([len(input_[0]),5,1], dtype=numpy.int32), activateFunc=cv2.ANN_MLP_SIGMOID_SYM, fparam1=1, fparam2=1)
+
+	rr = response_ -1 + response_
+	network.train(inputs=input_, outputs=rr, sampleWeights=numpy.ones(len(response_), dtype = numpy.float32), params=networkParams)
+
+	return network
+
+
+##################################################
+def evalClassifier(classifier_, data_, resp_, showMatrix_ = True, showStats_ = True):
+	issvm = isinstance(classifier_, type(cv2.SVM()))
+	isbst = isinstance(classifier_, type(cv2.Boost()))
+	isnet = isinstance(classifier_, type(cv2.ANN_MLP()))
+
+	tp = 0
+	tn = 0
+	fp = 0
+	fn = 0
+	total = len(data_)
+
+	for i in range(total):
+		sample = numpy.array([data_[i]], dtype = numpy.float32)
+
+		# predict an output
+		pred = -1
+		if issvm:
+			dist = classifier_.predict(sample, returnDFVal=True)
+			label = classifier_.predict(sample, returnDFVal=False)
+			pred = int(label)
+			logger.debug('resp: %d - label: %.1f - dist: % .3f', resp_[i], label, dist)
+
+		elif isbst:
+			label = classifier_.predict(sample, returnSum=False)
+			votes = classifier_.predict(sample, returnSum=True)
+			pred = int(label)
+			logger.debug('resp: %d - label: %.1f - votes: % .3f', resp_[i], label, votes)
+
+		elif isnet:
+			dummy, out = classifier_.predict(sample)
+			pred = int(out < 0)
+			logger.debug('resp: %d - out: % .3f', resp_[i], out)
+
+		# accumulate stats
+		if pred == 0: # predicted 0
+			if resp_[i] == 0: 
+				tn = tn + 1
+			else:
+				fn = fn + 1
+		else: # predicted 1
+			if resp_[i] == 1: 
+				tp = tp + 1
+			else:
+				fp = fp + 1
+
+
+	realn = tn + fp
+	realp = fn + tp
+
+	# generate a confusion matrix
+	matrix = []
+	matrix.append('-------------------------------------')
+	matrix.append('|         | pred: 0 | pred: 1 | SUM |')
+	matrix.append('-------------------------------------')
+	matrix.append('| resp: 0 |   {:3n}   |   {:3n}   | {:3n} |'.format(tn, fp, tn + fp))
+	matrix.append('| resp: 1 |   {:3n}   |   {:3n}   | {:3n} |'.format(fn, tp, fn + tp))
+	matrix.append('-------------------------------------')
+	matrix.append('|   SUM   |   {:3n}   |   {:3n}   | {:3n} |'.format(tn + fn, fp + tp, total))
+	matrix.append('-------------------------------------')
+
+	stats = []
+	if showStats_:
+		with numpy.errstate(invalid='ignore'): # temporarily disable warnings
+			stats.append('     accuracy:    {: .3f}'.format((tp + tn) / numpy.float64(total)))
+			stats.append('     missclass:   {: .3f}'.format((fp + fn) / numpy.float64(total)))
+			stats.append('     TPR:         {: .3f}'.format(tp / numpy.float64(realp)))
+			stats.append('     FPR:         {: .3f}'.format(fp / numpy.float64(realn)))
+			stats.append('     specificity: {: .3f}'.format(tn / numpy.float64(realn)))
+			stats.append('     precision:   {: .3f}'.format(tp / numpy.float64(tp + fp)))
+			stats.append('     prevalence:  {: .3f}'.format(realp / numpy.float64(total)))
+
+
+	nmatrix = len(matrix)
+	nstats = len(stats)
+	rprt = '\n'
+	for i in range(nmatrix):
+		if showMatrix_:
+			rprt = rprt + matrix[i]
+		if showStats_ and i < nstats:
+			rprt = rprt + stats[i]
+		rprt = rprt + '\n'
+
+	logger.info(rprt)
+
+
+##################################################
+def plotData2D(data_, resp_):
+	cls0 = data_[resp_ == 0]
+	cls1 = data_[resp_ == 1]
+
+	plt.plot(cls0[:,0], cls0[:,1], 'bo')
+	plt.plot(cls1[:,0], cls1[:,1], 'rx')
+	plt.axis([0, nclusters + 1, -0.5, 3.5])
+	plt.show()
+
 
 ##################################################
 def testPredictions(svm_, svm_auto_, boost_, network_):
@@ -194,124 +353,6 @@ def testPredictions(svm_, svm_auto_, boost_, network_):
 	logger.info(table)
 
 
-
-##################################################
-def evalClassifier(classifier_, data_, resp_):
-	issvm = isinstance(classifier_, type(cv2.SVM()))
-	isbst = isinstance(classifier_, type(cv2.Boost()))
-	isnet = isinstance(classifier_, type(cv2.ANN_MLP()))
-
-	missmatch = 0
-
-	logger.info('...evaluating')
-	for i in range(len(data_)):
-		sample = numpy.array([data_[i]], dtype = numpy.float32)
-
-		if issvm:
-			dist = svm_.predict(sample, returnDFVal=True)
-			label = svm_.predict(sample, returnDFVal=False)
-
-		elif isbst:
-			boostLabel = boost_.predict(sample, returnSum=False)
-			boostVotes = boost_.predict(sample, returnSum=True)
-
-		elif isnet:
-			dummy, networkOut = network_.predict(sample)
-
-
-		missmatch = missmatch + int(abs(label - resp_[i]) < 1e-10)
-
-
-##################################################
-def trainSVM(input_, response_, basedir_):
-	logger.info('...training SVM')
-
-	svmParams = dict(kernel_type = cv2.SVM_RBF, svm_type = cv2.SVM_C_SVC, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001))
-
-	svm_auto = cv2.SVM()
-	svm_auto.train_auto(input_, response_, None, None, params=svmParams, k_fold=7)
-
-	svmAutoDist = svm_auto.predict(input_[0], returnDFVal=True)
-	svmAutoLabel = svm_auto.predict(input_[0], returnDFVal=False)
-	logger.info('dist: %.4f -- label: %f --  resp: %d', svmAutoDist, svmAutoLabel, response_[0])
-
-
-
-
-	svmParams['C'] = 100
-	svmParams['gamma'] = 2
-	svm = cv2.SVM()
-	svm.train(input_, response_, params=svmParams)
-
-	logger.info('Saving SVM to disk')
-	svm_auto.save(basedir_ + '_svm_auto.yaml')
-	svm.save(basedir_ + '_svm.yaml')
-
-	return [svm, svm_auto]
-
-
-##################################################
-def trainBoost(input_, response_, basedir_):
-	logger.info('...training boosted classifier')
-
-	boostParams = dict(boost_type = cv2.BOOST_REAL, weak_count = 100, weight_trim_rate = 0.95, cv_folds = 3, max_depth = 1)
-
-	boost = cv2.Boost()
-	boost.train(trainData=input_, tflag=cv2.CV_ROW_SAMPLE, responses=response_, params=boostParams, update=False)
-
-	logger.info('Saving boosting classifier to disk')
-	boost.save(basedir_ + '_boost.yaml')
-
-	return boost
-
-
-##################################################
-def trainNetwork(input_, response_, basedir_):
-	logger.info('...training neural network')
-	# networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_BACKPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001), bp_dw_scale=0.1, bp_moment_scale=0.1, flags=(cv2.ANN_MLP_UPDATE_WEIGHTS | cv2.ANN_MLP_NO_INPUT_SCALE))
-	
-	networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_BACKPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 5000, 0.000001), bp_dw_scale=0.1, bp_moment_scale=0.1)
-
-
-	# networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_RPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001), rp_dw0=0.1, rp_dw_plus=1.2, rp_dw_minus=0.5, rp_dw_min=0.1, rp_dw_max=50, flags=(cv2.ANN_MLP_UPDATE_WEIGHTS | cv2.ANN_MLP_NO_INPUT_SCALE))
-
-	# networkParams = dict(train_method=cv2.ANN_MLP_TRAIN_PARAMS_RPROP, term_crit=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10000, 0.0000000001), rp_dw0=0.1, rp_dw_plus=1.2, rp_dw_minus=0.5, rp_dw_min=0.1, rp_dw_max=50)
-
-
-	network = cv2.ANN_MLP()
-	network.create(layerSizes=numpy.array([len(input_[0]),3,5,3,1], dtype=numpy.int32), activateFunc=cv2.ANN_MLP_SIGMOID_SYM, fparam1=1, fparam2=1)
-
-	rr = response_ -1 + response_
-	network.train(inputs=input_, outputs=rr, sampleWeights=numpy.ones(len(response_), dtype = numpy.float32), params=networkParams)
-
-	logger.info('Saving neural network to disk')
-	network.save(basedir_ + '_network.yaml')
-
-	return network
-
-
-##################################################
-def plotData(train_, resp_):
-	cls0 = train_[resp_ == 0]
-	cls1 = train_[resp_ == 1]
-
-	plt.plot(cls0[:,0], cls0[:,1], 'bo')
-	plt.plot(cls1[:,0], cls1[:,1], 'rx')
-	plt.axis([0, nclusters + 1, -0.5, 3.5])
-	plt.show()
-
-
-##################################################
-def getBaseName(inputdir_, outdir_):
-	i = -1
-	fname = inputdir_.split('/')[i]
-	while len(fname) == 0:
-		i = i -1
-		fname = inputdir_.split('/')[i]
-
-	return outdir_ + fname
-
-
 ##################################################
 if __name__ == '__main__':
 	if (len(sys.argv) < 2):
@@ -336,28 +377,58 @@ if __name__ == '__main__':
 	os.system('mkdir -p ' + OUTPUT_DIR)
 	train, tresp, nfiles, nsplit = extractToCSV(trainDir, baseName + '.csv', getFields())
 	logger.info('Traversed %d files', nfiles)
-	logger.info('\t- %d completed', len(response))
-	logger.info('\t- %d successful', sum(response))
-	logger.info('\t- ratio: %f', float(sum(response)) / float(len(train)))
+	logger.info('\t- %d completed', len(tresp))
+	logger.info('\t- %d successful', sum(tresp))
+	logger.info('\t- ratio: %f', float(sum(tresp)) / float(len(train)))
+
+	train = numpy.array(train, dtype = numpy.float32)
+	tresp = numpy.array(tresp, dtype = numpy.float32)
+
+	# train classifiers
+	svm, svm_auto = trainSVM(train, tresp)
+	boost = trainBoost(train, tresp)
+	network = trainNetwork(train, tresp)
 
 
-	# train a classifier
-	logger.info('Preparing data')
-	tt = numpy.array(train, dtype = numpy.float32)
-	rr = numpy.array(response, dtype = numpy.float32)
-
-	svm, svm_auto = trainSVM(tt, rr, baseName)
-	boost = trainBoost(tt, rr, baseName)
-	network = trainNetwork(tt, rr, baseName)
-
-	# if TEST_PREDICTIONS:
-		# testPredictions(svm, svm_auto, boost, network)
-
-	# evalModels(svm, svm_auto, boost, network, valDir)
+	# evaluate classifiers
+	logger.info('*** TRAINING: stats SVM ***')
+	evalClassifier(svm, train, tresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
+	logger.info('*** TRAINING: stats SVM auto ***')
+	evalClassifier(svm_auto, train, tresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
+	logger.info('*** TRAINING: stats Boost ***')
+	evalClassifier(boost, train, tresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
+	logger.info('*** TRAINING: stats NeuralNetwork ***')
+	evalClassifier(network, train, tresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
 
 
-	# plot training data
-	# plotData(tt, rr)
+	# retrieve validation data
+	logger.info('Retrieving validation data')
+	val, vresp, nfiles, dummy = traverseDirectory(valDir, getFields())
+	logger.info('Traversed %d files', nfiles)
+	logger.info('\t- %d completed', len(vresp))
+	logger.info('\t- %d successful', sum(vresp))
+	logger.info('\t- ratio: %f', float(sum(vresp)) / float(len(val)))
+
+	val = numpy.array(val, dtype = numpy.float32)
+	vresp = numpy.array(vresp, dtype = numpy.float32)
+
+
+	# evaluate using validation data
+	logger.info('*** VALIDATION: stats SVM ***')
+	evalClassifier(svm, val, vresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
+	logger.info('*** VALIDATION: stats SVM auto ***')
+	evalClassifier(svm_auto, val, vresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
+	logger.info('*** VALIDATION: stats Boost ***')
+	evalClassifier(boost, val, vresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
+	logger.info('*** VALIDATION: stats NeuralNetwork ***')
+	evalClassifier(network, val, vresp, showMatrix_ = SHOW_MATRIX, showStats_ = SHOW_STATS)
+
+
+	logger.info('Saving classifiers to disk')
+	svm_auto.save(baseName + '_svm_auto.yaml')
+	svm.save(baseName + '_svm.yaml')
+	boost.save(baseName + '_boost.yaml')
+	network.save(baseName + '_network.yaml')
 
 
 	logger.info('Execution finished')
